91f28554ec4fb233ef6dc559ee9c78af01f0b610
iloveu777 <116212823+iloveu777@users.noreply.github.com>
2023-03-06 08:02:03 +0200

    Add files via upload

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Console;
+using System.Net.NetworkInformation;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Title = "получение значений для массива из файла";
+            string filePath = @"C:\mass.txt"; // Указываем путь к файлу
+            string[] lines = File.ReadAllLines(filePath); // Читаем все строки из файла в массив строк
+            int[] values = Array.ConvertAll(lines, int.Parse); // Преобразуем массив строк в массив целых чисел
+            foreach(int value in values)
+            {
+                Console.WriteLine(value);
+            } */
+            string a = "abcdабцд";
+            int ab = 12;
+            byte[] mas = {1, 11, 13, 5, };
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "example.txt");
+
+            using (StreamWriter writer = File.CreateText(filePath))
+            {
+                for(int i = 0; i < mas.Length; i++)
+                {
+                    writer.WriteLine(mas[i]);
+                }
+            }
+            WriteLine("записано.");
+         /*    for (int i = 0; i < mas.Length; i++)
+             {
+                 file.Write($"{mas[i]}\n");
+             }
+            
+
+
+             /*   string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "example.txt");
+                using (StreamWriter writer = File.CreateText(filePath))
+                {
+                    writer.Write("123");
+                }
+                Console.WriteLine("Файл успешно записан.");
+            */
+      
+
+        ReadKey();
+        }
+    }
+}
